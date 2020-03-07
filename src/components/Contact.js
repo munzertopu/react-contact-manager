a3cb71e8f305ca1b642e549107b61bbb3bd48a87
munzertopu <mmorshed@paradynamix.com>
2020-03-07 22:48:20 +0600

    Upto toggle state

---
+++ src/components/Contact.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Contact extends Component {
+  state = {
+    showContactInfo: true
+  };
+  OnShowClick = () => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+  render() {
+    const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {name} <i onClick={this.OnShowClick} className="fas fa-sort-down"></i>{" "}
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email : {email}</li>
+            <li className="list-group-item">Phone : {phone}</li>
+          </ul>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default Contact;
